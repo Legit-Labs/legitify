c7e0d8cbd4279e20ef9a907a9d668a89cdc56b84
noamd <noam@legitsecurity.com>
2022-11-02 18:11:33 +0200

    added runner group collection and a runner group policy

---
+++ internal/collectors/runners_collector.go
@@ -0,0 +1,101 @@
+package collectors
+
+import (
+	ghcollected "github.com/Legit-Labs/legitify/internal/collected/github"
+	"github.com/Legit-Labs/legitify/internal/common/group_waiter"
+	"github.com/Legit-Labs/legitify/internal/common/permissions"
+	"github.com/google/go-github/v44/github"
+	"log"
+
+	ghclient "github.com/Legit-Labs/legitify/internal/clients/github"
+	"github.com/Legit-Labs/legitify/internal/common/namespace"
+	"golang.org/x/net/context"
+)
+
+type runnersCollector struct {
+	baseCollector
+	client  ghclient.Client
+	context context.Context
+	cache   map[string][]*github.RunnerGroup
+}
+
+func newRunnersCollector(ctx context.Context, client ghclient.Client) collector {
+	c := &runnersCollector{
+		client:  client,
+		context: ctx,
+		cache:   make(map[string][]*github.RunnerGroup),
+	}
+	initBaseCollector(&c.baseCollector, c)
+	return c
+}
+
+func (c *runnersCollector) Namespace() namespace.Namespace {
+	return namespace.Runners
+}
+
+func (c *runnersCollector) CollectMetadata() Metadata {
+	gw := group_waiter.New()
+	orgs, err := c.client.CollectOrganizations()
+	if err != nil {
+		log.Printf("failed to collection organizations %s", err)
+		return Metadata{}
+	}
+
+	totalCount := 0
+	for _, org := range orgs {
+		gw.Do(func() {
+			org := org
+			result := make([]*github.RunnerGroup, 0)
+			err := ghclient.PaginateResults(func(opts *github.ListOptions) (*github.Response, error) {
+				runners, resp, err := c.client.Client().Actions.ListOrganizationRunnerGroups(c.context, *org.Login, opts)
+
+				if err != nil {
+					log.Printf("err collection runner groups for %s - %v", *org.Login, err)
+					return nil, err
+				}
+
+				result = append(result, runners.RunnerGroups...)
+				return resp, nil
+			})
+
+			if err != nil {
+				log.Printf("Failed to collected runner group metadata for organization %s", *org.Login)
+			} else {
+				c.cache[*org.Login] = result
+				totalCount = totalCount + len(result)
+			}
+		})
+	}
+
+	gw.Wait()
+	return Metadata{
+		totalCount,
+	}
+}
+
+func (c *runnersCollector) Collect() subCollectorChannels {
+	return c.wrappedCollection(func() {
+		orgs, err := c.client.CollectOrganizations()
+
+		if err != nil {
+			log.Printf("failed to collect organizations %s", err)
+			return
+		}
+
+		for _, org := range orgs {
+			cached := c.cache[*org.Login]
+
+			for _, rg := range cached {
+				c.collectionChangeByOne()
+
+				c.collectData(org,
+					ghcollected.RunnerGroup{
+						Organization: org,
+						RunnerGroup:  rg,
+					},
+					*org.HTMLURL,
+					[]permissions.Role{org.Role})
+			}
+		}
+	})
+}
---
+++ policies/github/runner.rego
@@ -0,0 +1,25 @@
+package runner
+
+# METADATA
+# scope: rule
+# title: Runner group is not limited to private repositories
+# description: |
+#       Workflows from public repositories are allowed to run on GitHub Hosted Runners.
+#       When using GitHub Hosted Runners, it is recommended to allow only workflows from private repositories to run on these runners to avoid being vulnerable
+#       to malicious actors using workflows from public repositories to break into your private network.
+#       In case of inadequate security measures implemented on the hosted runner,
+#       malicious actors could fork your repository and then pwn-requests (Create a pull-request from a forked repository to the base repository with malicious intentions)
+#       that create a workflow that exploits these vulnerabilities and move laterally inside your network.
+# custom:
+#   severity: HIGH
+#   requiredEnrichers: [organizationId]
+#   requiredScopes: [admin:org]
+#   remediationSteps:
+#     - "Go to the organization settings page"
+#     - "Press Actions ➝ Runner groups"
+#     - "Select the violating repository"
+#     - "Uncheck Allow public repositories"
+default public_repos_can_user_runner_group = false
+public_repos_can_user_runner_group {
+    input.runner_group.allows_public_repositories == true
+}
