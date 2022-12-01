921f65a16769f6a573e81949318c73108d32c2ac
nadav-legit <107790206+nadav-legit@users.noreply.github.com>
2022-12-01 14:29:28 +0200

    feat: added github organization, actions and runner groups tests (#57)
    * added github organization, actions and runner groups tests
    
    Co-authored-by: noamd-legit <74864790+noamd-legit@users.noreply.github.com>

---
+++ e2e/e2e_test.go
@@ -0,0 +1,93 @@
+package test
+
+import (
+	"flag"
+	"github.com/thedevsaddam/gojsonq/v2"
+	"testing"
+)
+
+var reportPath = flag.String("report_path", "/tmp/out.json", "legitify report output path")
+
+func TestGitHub(t *testing.T) {
+	tests := []struct {
+		path         string
+		failedEntity string
+		passedEntity string
+	}{
+		// actions tests
+		{
+			path:         "data.actions.token_default_permissions_is_read_write",
+			failedEntity: "Legitify-E2E-2",
+			passedEntity: "Legitify-E2E",
+		},
+		{
+			path:         "data.actions.all_repositories_can_run_github_actions",
+			failedEntity: "Legitify-E2E-2",
+			passedEntity: "Legitify-E2E",
+		},
+		{
+			path:         "data.actions.all_github_actions_are_allowed",
+			failedEntity: "Legitify-E2E-2",
+			passedEntity: "Legitify-E2E",
+		},
+		{
+			path:         "data.actions.actions_can_approve_pull_requests",
+			failedEntity: "Legitify-E2E-2",
+			passedEntity: "Legitify-E2E",
+		},
+		// runner group tests
+		{
+			path:         "data.runner_group.runner_group_can_be_used_by_public_repositories",
+			failedEntity: "test fail",
+			passedEntity: "test pass",
+		},
+		{
+			path:         "data.runner_group.runner_group_not_limited_to_selected_repositories",
+			failedEntity: "test fail",
+			passedEntity: "test pass",
+		},
+		// organization tests
+		{
+			path:         "data.organization.non_admins_can_create_public_repositories",
+			failedEntity: "Legitify-E2E-2",
+			passedEntity: "Legitify-E2E",
+		},
+		{
+			path:         "data.organization.default_repository_permission_is_not_none",
+			failedEntity: "Legitify-E2E-2",
+			passedEntity: "Legitify-E2E",
+		},
+		{
+			path:         "data.organization.two_factor_authentication_not_required_for_org",
+			failedEntity: "Legitify-E2E-2",
+			passedEntity: "Legitify-E2E",
+		},
+		{
+			path:         "data.organization.organization_webhook_no_secret",
+			failedEntity: "Legitify-E2E-2",
+			passedEntity: "Legitify-E2E",
+		},
+		{
+			path:         "data.organization.organization_webhook_doesnt_require_ssl",
+			failedEntity: "Legitify-E2E-2",
+			passedEntity: "Legitify-E2E",
+		},
+	}
+	for _, test := range tests {
+		testFormattedPath := test.path + "->violations"
+		jq := gojsonq.New(gojsonq.SetSeparator("->")).File(*reportPath)
+		res := jq.From(testFormattedPath).Where("aux->entityName", "=", test.passedEntity).Where("Status", "=", "PASSED").Count()
+
+		if res != 1 {
+			t.Logf("Failed on test %s, Entity %s did not pass", test.path, test.passedEntity)
+			t.Fail()
+		}
+		jq = gojsonq.New(gojsonq.SetSeparator("->")).File(*reportPath)
+		res = jq.From(testFormattedPath).Where("aux->entityName", "=", test.failedEntity).Where("Status", "=", "FAILED").Count()
+
+		if res != 1 {
+			t.Logf("Failed on test: %s, Entity: %s did not failed", test.path, test.failedEntity)
+			t.Fail()
+		}
+	}
+}
