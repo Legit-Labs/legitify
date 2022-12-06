c3a8a1bb30eb58bf4b40e48544ee6c48d08bc316
noamd <noam@legitsecurity.com>
2022-12-06 15:09:06 +0200

    fixed pr issue

--- internal/clients/gitlab/utils.go
+++ internal/clients/gitlab/utils.go
@@ -13,7 +13,7 @@ func PaginateResults(api func(opts *gitlab.ListOptions) (*gitlab.Response, error
 			return err
 		}
 
-		if resp.NextPage == 0 {
+		if resp.CurrentPage == resp.TotalPages {
 			return nil
 		}
 
