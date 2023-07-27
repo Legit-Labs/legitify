f1a45d16a8d4d8271c49d5d749372ca29569814f
Neta Spektor <neta@legitsecurity.com>
2023-07-27 14:35:01 +0300

    fixed query error

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -94,9 +94,8 @@ func cliTestLoop(t *testing.T, cliTests []cliTestCase) {
 		}
 		t.Logf("Testing: %s", cliTest.legitifyCommand)
 		jq.Reset()
-		content := jq.From("content")
-		failedTest := content.Where(cliTest.field, cliTest.op, cliTest.value)
-		count := failedTest.Count()
+		content := jq.From("Content->content")
+		count := content.Where(cliTest.field, cliTest.op, cliTest.value).Count()
 		if count != 0 {
 			t.Logf("Failed on test %s", cliTest.legitifyCommand)
 			t.Fail()
