e192c5d7bdc0a19b29872018830ccb8ceeb108bf
noamd <noam@legitsecurity.com>
2023-11-26 14:18:07 +0200

    align left

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { context, getOctokit } = require("@actions/github");
 const artifact = require("@actions/artifact");
 const { exit } = require("process");
 
-const logoMarkdown = `<div align="center">
+const logoMarkdown = `<div align="left">
 <a href="https://www.legitsecurity.com">
  <img width="120" alt="Legitify Logo" src="https://github.com/Legit-Labs/legitify/assets/74864790/c76dc765-e8fd-498e-ab92-1228eb5a1f2d">
  </a>
